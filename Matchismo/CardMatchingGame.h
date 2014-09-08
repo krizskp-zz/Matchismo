1b1380fc63b253028ddca9100027f8cdc00797a5
krizskp <shalvindra@outlook.com>
2014-09-08 14:35:48 +1200

    Added 12 decks of cards

---
+++ Matchismo/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Shalvindra Prasad on 9/8/14.
+//  Copyright (c) 2014 iOS Tutorial. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Shalvindra Prasad on 9/8/14.
+//  Copyright (c) 2014 iOS Tutorial. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
